6f1fd735b339404fe92a327d301ce12ffd5357b6
adarlpz <adarlopz.dev@gmail,com>
2025-04-23 21:32:42 -0600

    es un desmadre ahorita, necesitamos arreglar el public con los recursos, agrupar elementos en componentes y en buenas noticias los colores ya estan implementados :)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                //primary: '#18cda9',
+                //secondary: '#198980',
+                //neutral: '#2d504a',
+                //esos de arriba son los del texto pero siempre usamos negro para el texto xd
+
+                background: '#D4D1F0',
+                backgroundPrimary: '#B8B2E6',
+                backgroundSecondary: '#9C93DC',
+            },
+        },
+    },
+    plugins: [],
 };
