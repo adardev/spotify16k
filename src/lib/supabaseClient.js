bb0d6eb7235c921d43eb468c9272d19ceea7ce51
unknown <a22100190@ceti.mx>
2025-06-03 17:43:24 -0600

    conexion con supabase exitosa, revisar  endpoint.astro porfa

--- src/lib/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// src/lib/supabaseClient.js
-import postgres from 'postgres'
-
-// ❗Este código solo debe usarse en el servidor (como en endpoints)
-const connectionString = 'postgresql://postgres:Spotify16k1234@db.nekpeywnfddgehftmquk.supabase.co:5432/postgres'
-
-const sql = postgres(connectionString)
-
-export default sql
