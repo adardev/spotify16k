aa981d041b2095fee3061cd7bdf0314caf547f6d
adarlpz <adarlopz.dev@gmail,com>
2025-06-04 23:32:29 -0600

    insert usuario

--- src/pages/api/endpoint.ts
+++ src/pages/api/endpoint.ts
@@ -3,25 +3,14 @@ import type { APIContext } from "astro";
 import { supabase } from "../../db/supabase";
 
 export async function POST({ request }: APIContext) {
-  const { nombre, spotify_id } = await request.json();
+    console.log("POST /api/endpoint llamado");
+  const { nombre, spotify_id, contraseña } = await request.json();
 
-  const { data: existingUser } = await supabase
+  const { data, error } = await supabase
     .from("usuario")
-    .select("*")
-    .eq("spotify_id", spotify_id)
-    .single();
+    .insert([{ nombre, spotify_id, contraseña }]);
 
-  if (!existingUser) {
-    const { data, error } = await supabase
-      .from("usuario")
-      .insert([{ nombre, spotify_id }]);
-
-    return new Response(JSON.stringify({ inserted: true, error }), {
-      status: error ? 500 : 200,
-    });
-  }
-
-  return new Response(JSON.stringify({ inserted: false, user: existingUser }), {
-    status: 200,
+  return new Response(JSON.stringify({ inserted: true, error }), {
+    status: error ? 500 : 200,
   });
 }
